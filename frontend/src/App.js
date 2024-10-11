c68f07e37859ebc6b6b874fea5efed0d9a505224
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-11 15:55:35 -0700

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
-const App = () => {
+
+const App = () => {  
+
+  const [word, setWord] = useState('');
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+    console.log(word);
+  }
+  
+
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 };
