3f29aa63b822e0ab9db988426105de6b405ff848
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-11 09:51:46 -0700

    initial header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
-const Header  = () => {
+const Header  = ({ title }) => {
     return (
         <Navbar bg="dark" data-bs-theme="light">
-        <Container>
-            <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
-        </Container>
-      </Navbar>
+            <Container>
+                <Navbar.Brand href="/">{title}</Navbar.Brand>
+            </Container>
+        </Navbar>
     )
 };
 
 export default Header;
-
-
-
-
-
-
