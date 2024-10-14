e7729c63eb0ac165c0ff521eab32417ddd48c33b
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-10-13 21:35:52 -0700

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images);
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(e.target[0].value);
@@ -16,7 +20,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
